f253c73b9ba0e116fd8c694a563a9d9fc4e10153
penja <a.penchuk@scalr.com>
2021-06-04 18:33:16 +0300

    use module from git

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+module "test" {
+  source = "git::https://github.com/jfblg/terraform-random-jarm",
+  random_length=24
+}
+
 resource "random_string" "name" {
   length = var.length - length(var.prefix)
   keepers = var.keepers
