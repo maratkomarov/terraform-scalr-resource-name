31c6bd4018ea1562e0b8b6da71cba64208123dfe
Andrii Penchuk <penja7@gmail.com>
2021-08-09 14:03:17 +0300

    Update main.tf

--- workspace/main.tf
+++ workspace/main.tf
@@ -2,6 +2,12 @@ module "resource-name" {
   source  = "penja.scalr.io/env-t4a5m9gqfptdmtg/resource-name/scalr"
   version = "4.0.10"
 }
+  
+resource "null_resource" "cluster" {
+  triggers = {
+    timestamp = timestamp()
+  }
+}
 
 output "test" {
   value = "test"
