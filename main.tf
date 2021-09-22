99cf4922561d288e361ac2a882fb90d2e380a20a
Andrii Penchuk <penja7@gmail.com>
2021-09-22 13:08:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ module "test" {
   source = "git::https://github.com/jfblg/terraform-random-jarm"
   random_length=24
 }
+  
+module "test2" {
+  source = "git::https://github.com/penja/sc-offering-sc//work_dir"
+}
 
 resource "random_string" "name" {
   length = var.length - length(var.prefix)
