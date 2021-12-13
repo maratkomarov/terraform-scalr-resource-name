3bd5ff1b4ed22e66381e93017a6ade0f6f70966a
Marat Komarov <marat@scalr.com>
2021-12-13 13:31:42 -0800

    Adding one more resource

--- main.tf
+++ main.tf
@@ -12,9 +12,13 @@ resource "random_string" "name2" {
   length = var.length - length(var.prefix)
   keepers = var.keepers
 }
+  
+resource "random_string" "name3" {
+  length = var.length - length(var.prefix)
+  keepers = var.keepers
+}
 
 output "name" {
   value = "${var.prefix}${random_string.name.id}"
   description = "The generated resource name"
 }
-
